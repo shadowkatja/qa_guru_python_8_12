728f0f8e12621dd8759296526d86e944f35c5eea
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-26 14:42:17 +0300

    remove adds

--- tests_demo/pages/registration_page.py
+++ tests_demo/pages/registration_page.py
@@ -10,6 +10,10 @@ class RegistrationPage:
     def open_form(self):
         browser.open('/automation-practice-form')
         browser.execute_script('document.querySelector(".body-height").style.transform = "scale(.75)"')
+        browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
+            have.size_greater_than_or_equal(3)
+        )
+        browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
         return self
 
     def fill_date_of_birth(self, user:User):
